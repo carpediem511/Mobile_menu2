fbb1846f4e131b7c46e906992698594b365f4657
carpediem511 <vampire.0514@gmail.com>
2022-08-15 16:19:34 +0300

    готово

--- script.js
+++ script.js
@@ -1,56 +1,78 @@
 
-   
 let isOpen1 = false
 let isOpen2 = false
 let isOpen3 = false
 
+function openMenu1() {
 
-function openMenu() {
+   let changeIcon1 = document.getElementById("icon-menu1")
+   let openMenu1 = document.getElementById("title1OpenMenu")
+   let button1 = document.getElementById("title1")
 
-  let changeIcon1 = document.getElementById("icon-menu1")
-  let openMenu1 = document.getElementById("title1OpenMenu")
-  let changeIcon2 = document.getElementById("icon-menu2")
-  let openMenu2 = document.getElementById("title2OpenMenu")
-  let changeIcon3 = document.getElementById("icon-menu3")
-  let openMenu3 = document.getElementById("title3OpenMenu")
-  
-  if (isOpen1 & isOpen2 & isOpen3) {
-   openMenu1.style.display = "none"
-   openMenu2.style.display = "none"
-   openMenu3.style.display = "none"
-   changeIcon1.src = "arrow_down_icon.png"
-   changeIcon2.src = "arrow_down_icon.png"
-   changeIcon3.src = "arrow_down_icon.png"
-
-   isOpen1 = false
-   isOpen2 = false
-   isOpen3 = false
-  }
-
-  else if (isOpen1) {
-    openMenu1.style.display = "flex"
-    changeIcon1.src = "arrow_up_icon.png"
  
-    isOpen1 = true
-    isOpen2 = false
-    isOpen3 = false
-   }
- 
-   else if (isOpen2) {
-    openMenu2.style.display = "flex"
-    changeIcon2.src = "arrow_up_icon.png"
-
-    isOpen1 = false
-    isOpen2 = true
-    isOpen3 = false
-   }
-
-   else if (isOpen3) {
-    openMenu3.style.display = "flex"
-    changeIcon3.src = "arrow_up_icon.png"
-
-    isOpen1 = false
-    isOpen2 = false
-    isOpen3 = true
-   }
+   if (button1) { // если у нас нажали на первый элемент 
+		if (isOpen1) { // если он уже был открыт, значит, нажали еще раз - закрываем
+			// поменять иконку
+            openMenu1.style.display = "none"
+            changeIcon1.src = "arrow_down_icon.png"
+			isOpen1 = false // установить закрытым
+   
+		} else {
+			// поменять иконку	
+         openMenu1.style.display = "flex"
+         changeIcon1.src = "arrow_up_icon.png"
+			isOpen1 = true // установить открытым
+        
+		}
+     
+	}
 }
+
+function openMenu2() {
+
+   let changeIcon2 = document.getElementById("icon-menu2")
+   let openMenu2 = document.getElementById("title2OpenMenu")
+   let button2 = document.getElementById("title2")
+
+   if (button2) { // если у нас нажали на первый элемент 
+		if (isOpen2) { // если он уже был открыт, значит, нажали еще раз - закрываем
+			// поменять иконку
+         openMenu2.style.display = "none"
+            changeIcon2.src = "arrow_down_icon.png"
+			isOpen2 = false // установить закрытым
+		} else {
+			// поменять иконку	
+         openMenu2.style.display = "flex"
+         changeIcon2.src = "arrow_up_icon.png"
+			isOpen2 = true // установить открытым
+		}
+     
+	}
+
+}
+
+function openMenu3() {
+
+let button3 = document.getElementById("title3")
+let changeIcon3 = document.getElementById("icon-menu3")
+let openMenu3 = document.getElementById("title3OpenMenu")
+
+   if (button3) { // если у нас нажали на первый элемент 
+		if (isOpen3) { // если он уже был открыт, значит, нажали еще раз - закрываем
+			// поменять иконку
+            openMenu3.style.display = "none"
+            changeIcon3.src = "arrow_down_icon.png"
+			isOpen3 = false // установить закрытым
+		} else {
+			// поменять иконку	
+         openMenu3.style.display = "flex"
+         changeIcon3.src = "arrow_up_icon.png"
+			isOpen3 = true // установить открытым
+		}
+     
+	}
+
+}
+
+
+  
